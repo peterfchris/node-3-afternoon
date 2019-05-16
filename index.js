26acc43e804e4d50fe209ff502b4186ec224f5eb
Peter Forsgren Christensen <peter.f.chris@gmail.com>
2019-05-16 15:09:16 -0600

    step 7 done and bugs fixed

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-require('env').config()
+require('dotenv').config()
 const express = require('express')
-const massive = massive('massive')
-const {SERVER_PORT, CONNECTION_STRING} = proces.env
+const massive = require('massive')
+const {SERVER_PORT, CONNECTION_STRING} = process.env
 const app = express()
+const products_controller = require('./products_controller')
 
 massive(CONNECTION_STRING)
     .then((database) => {
         app.set('db', database)
+    })
     .catch((err) => {
         console.log(err)
     })
-})
+
 
 app.use(express.json())
 
-app.post('/api/products', products_controller.create)
+app.post('/api/addproduct', products_controller.create)
 app.get('/api/getone/:id', products_controller.getOne)
 app.get('/api/products', products_controller.getAll)
 app.put('/api/products/:id', products_controller.update)
