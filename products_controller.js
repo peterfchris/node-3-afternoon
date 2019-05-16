46ee867a7cd52be0e6aa9837bf60f84ab205c1e7
Peter Forsgren Christensen <peter.f.chris@gmail.com>
2019-05-16 14:54:37 -0600

    Step 6 done

--- products_controller.js
+++ products_controller.js
@@ -1,12 +1,14 @@
 module.exports = {
     create: (req, res) => {
         const db = req.app.get('db')
+        const {product_name, description, price, image_url} = req.body
         db.create_product({$1, $2, $3, $4}).then(() => {
             res.sendStatus(200)
         })
     },
     getOne: (req, res) => {
         const db = req.app.get('db')
+        const {id} = req.params
         db.read_product()
         .then((product) => {
             res.status(200).send(product)
@@ -27,6 +29,7 @@ module.exports = {
     },
     update: (req, res) => {
         const db = req.app.get('db')
+        const {params, query} = req 
         db.update_product()
         .then(() => {
             res.sendStatus(200)
@@ -37,6 +40,7 @@ module.exports = {
     },
     delete: (req, res) => {
         const db = req.app.get('db')
+        const {id} = req.params
         db.delete_product()
         .then(() => {
             res.sendStatus(200)
