438d0792fb3792ac2d7e67a64328c9f12433bcae
Peter Forsgren Christensen <peter.f.chris@gmail.com>
2019-05-16 14:01:53 -0600

    step 2 done

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 require('env').config()
 const express = require('express')
 const massive = massive('massive')
-const {SERVER_PORT} = proces.env
+const {SERVER_PORT, CONNECTION_STRING} = proces.env
 const app = express()
+
+massive(CONNECTION_STRING)
+    .then((database) => {
+        app.set('db', database)
+    .catch((err) => {
+        console.log(err)
+    })
+})
 
 app.listen(SERVER_PORT, () => console.log(`Nothing is screwed up on ${SERVER_PORT}`))
